6332f31ed55fba44700480f19fc4afc3e76241f7
guilin <xuguilin@muyungroup.cn>
2025-09-17 10:45:52 +0800

    网络请求依赖，新增解析模型

---
+++ app/src/main/java/cn/guilin/baseframeworkcompose/model/NetworkPageData.kt
@@ -0,0 +1,13 @@
+package cn.guilin.baseframeworkcompose.model
+
+data class NetworkPageData<T>(
+    /**
+     * 列表
+     */
+    val list: List<T>? = null,
+
+    /**
+     * 分页数据
+     */
+    val pagination: NetworkPageMeta? = null,
+)
---
+++ app/src/main/java/cn/guilin/baseframeworkcompose/model/NetworkPageMeta.kt
@@ -0,0 +1,23 @@
+package cn.guilin.baseframeworkcompose.model
+
+import kotlinx.serialization.Serializable
+/**
+ * 分页模型
+ */
+@Serializable
+data class NetworkPageMeta(
+    /**
+     * 总条数
+     */
+    val total: Int? = null,
+
+    /**
+     * 每页显示条数
+     */
+    val size: Int? = null,
+
+    /**
+     * 当前页码
+     */
+    val page: Int? = null
+)
