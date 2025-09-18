b392d70c85d9c918c37a9c0991281ab0238fb198
guilin <xuguilin@muyungroup.cn>
2025-09-18 18:08:29 +0800

    DataSourceModule修改

--- core/network/src/main/java/cn/guilin/framework/core/network/di/DataSourceModule.kt
+++ core/network/src/main/java/cn/guilin/framework/core/network/di/DataSourceModule.kt
@@ -4,32 +4,28 @@ import cn.guilin.framework.core.network.datasource.banner.BannerDataSource
 import cn.guilin.framework.core.network.datasource.banner.BannerDataSourceImpl
 import cn.guilin.framework.core.network.datasource.goods.GoodsDataSource
 import cn.guilin.framework.core.network.datasource.goods.GoodsDataSourceImpl
-import dagger.Binds
+import cn.guilin.framework.core.network.service.BannerService
+import cn.guilin.framework.core.network.service.GoodsService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class DataSourceModule {
+object DataSourceModule {
 
-    /**
-     * 将 GoodsDataSource 接口与其具体实现类 GoodsDataSourceImpl 进行绑定，
-     * 使得当需要 GoodsDataSource 实例时，
-     * Hilt会自动提供 GoodsDataSourceImpl 的单例实例。
-     */
-    @Binds//表示这是一个绑定函数，用于将实现类绑定到接口
+    @Provides
     @Singleton
     //定义一个抽象函数，用于绑定数据源接口和实现类
-    abstract fun bindGoodsDataSource(
-        impl: GoodsDataSourceImpl
+    fun provideGoodsDataSource(goodsService: GoodsService): GoodsDataSource {
+        return GoodsDataSourceImpl(goodsService)
+    }
 
-    ): GoodsDataSource
-
-    @Binds
+    @Provides
     @Singleton
-    abstract fun bindBannerDataSource(
-        impl: BannerDataSourceImpl
-    ): BannerDataSource
+    fun provideBannerDataSource(bannerService: BannerService): BannerDataSource {
+        return BannerDataSourceImpl(bannerService)
+    }
 }
