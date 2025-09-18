5228d5b2c4ce75b9b7477a454e68c5c1c87889df
guilin <xuguilin@muyungroup.cn>
2025-09-18 10:21:36 +0800

    新建 feature 模块

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,10 +30,29 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "BaseFrameworkCompose"
+
 include(":app")
+// core 模块
 include(":core:ui")
 include(":core:model")
 include(":core:network")
 include(":core:designsystem")
 include(":core:data")
 include(":core:common")
+// feature 模块
+// 首页
+include(":feature:mian")
+// 用户认证
+include(":feature:auth")
+// 公共
+include(":feature:common")
+// 商品
+include(":feature:goods")
+// 启动流程
+include(":feature:launch")
+// 营销
+include(":feature:market")
+// 订单
+include(":feature:order")
+// 用户
+include(":feature:user")
